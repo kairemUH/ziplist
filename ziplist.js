4c6fc43831c9fae5c617f4bf2b66d45acad22a43
kairemUH <kairem@hawaii.edu>
2023-09-18 22:53:43 -1000

    WOD commit

---
+++ ziplist.js
@@ -0,0 +1,23 @@
+function zipList(array1, array2) {
+  if (array1.length !== array2.length) {
+    return -1;
+  }
+
+  const returnArray = [];
+
+  for (let i = 0; i < array1.length; i++) {
+    returnArray.push(array1[i]);
+    returnArray.push(array2[i]);
+  }
+  return returnArray;
+}
+
+function zipListTheSimpleWay(array1, array2) {
+  return _.flatten(_.map(array1, function (item, index) { return [item, array2[index]]; }));
+}
+
+const testArr1 = ['a', 'b', 'c'];
+const testArr2 = [1, 2, 3];
+
+console.log(zipList(testArr1, testArr2));
+console.log(zipListTheSimpleWay(testArr1, testArr2));
